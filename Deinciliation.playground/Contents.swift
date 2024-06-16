488461a43e4a7195aeece5ef1466dc1ad4edbeed
Tolik38 <trostinavusa@gmail.com>
2024-06-15 20:14:03 -0700

    06/15/24

--- Deinciliation.playground/Contents.swift
+++ Deinciliation.playground/Contents.swift
@@ -1,3 +1,31 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+class Boomb{
+    var damage = 50
+    
+    // ARC
+    deinit{
+        print("Boom!")
+    }
+}
+
+class AirBoomb: Boomb{
+    var speed:Double = 80.0
+    deinit {
+        print("Air Boomb!")
+    }
+}
+
+do{
+    var boomb = Boomb()
+    print("end of DO scope")
+}
+
+print("--------Playground scope--------")
+
+var boombNil:AirBoomb? = AirBoomb()
+boombNil?.speed
+boombNil?.damage
+boombNil = nil
---
+++ Extensions.playground/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+class Person {
+    func sayHi () {
+        print("Hi")
+    }
+}
+
+extension Person {
+    func sayHello(){
+        print("Hello")
+    }
+}
+
+extension Person{
+    
+    static var avgLifeDuration = 75   // static
+    
+    func sayBlaBla(){
+        print("BlaBlaBla")
+    }
+}
+
+class SmarPerson: Person{
+    func saySmart(){
+        print("I'm really smart!")
+    }
+    
+    override func sayHi() {         // override is not working from extentions *** or you can add "@objc dynamic" for override target
+        print("Hi, I'm smart man!")
+    }
+    
+}
+
+var human = Person()
+human.sayHi()
+human.sayBlaBla()
+var smartHuman = SmarPerson()
+smartHuman.saySmart()
+smartHuman.sayHi()
+
+Person.avgLifeDuration = 88
+SmarPerson.avgLifeDuration = 99
+Person.avgLifeDuration
